231e35682f2aae4bf522e2cead8a0d8181272848
wenyutang-ms <wenyutang@microsoft.com>
2024-12-27 17:19:26 +0800

    feat: update

--- evalprompt/generateApiSpec.py
+++ evalprompt/generateApiSpec.py
@@ -17,7 +17,7 @@ from typing import List, Tuple, TypedDict
 import json
 from dotenv import load_dotenv
 import subprocess
-load_dotenv('..')
+load_dotenv()
 # Create a session for making HTTP requests
 session = requests.Session()
 
@@ -25,6 +25,23 @@ session = requests.Session()
 templateLoader = jinja2.FileSystemLoader(pathlib.Path(__file__).parent.resolve())
 templateEnv = jinja2.Environment(loader=templateLoader)
 user_message_template2 = templateEnv.get_template("user-message2.jinja2")
+
+def remove_markdown(text):
+    # 移除Markdown链接
+    text = re.sub(r'\[([^\]]+)\]\([^\)]+\)', r'\1', text)
+    # 移除Markdown粗体
+    text = re.sub(r'\*\*([^\*]+)\*\*', r'\1', text)
+    # 移除Markdown斜体
+    text = re.sub(r'\*([^\*]+)\*', r'\1', text)
+    # 移除Markdown列表
+    text = re.sub(r'^\s*-\s', '', text, flags=re.MULTILINE)
+    return text
+
+def remove_code_block_format(text):
+    # 移除代码块的开始和结束标记
+    text = re.sub(r'^```yaml.*\n', '', text, flags=re.MULTILINE)
+    text = re.sub(r'\n```$', '', text, flags=re.MULTILINE)
+    return text
 
 def get_api_spec(query: str) -> str:
     # Get the API spec from the OpenAI API
@@ -62,13 +79,51 @@ def get_language_from_extension(extension: str) -> str:
         "cpp": "c++"
     }
     return extension_to_language.get(extension, "unknown")
-def run_npm_script(filename: str, output_dir: str) -> str:
+def run_npm_script(filename: str, file_out: str, output_dir: str) -> str:
+    print("Running npm script", filename)
     try:
-        result = subprocess.run(["spectral", "lint", filename, "--ruleset", "https://raw.githubusercontent.com/Azure/APICenter-Analyzer/preview/resources/rulesets/oas.yaml"], shell=True, check=True, capture_output=True, text=True, cwd=output_dir)
-        print("Output:", result.stdout)
+        result = subprocess.run(["spectral", "lint", filename, "--ruleset", "https://raw.githubusercontent.com/Azure/APICenter-Analyzer/preview/resources/rulesets/oas.yaml" , "--format", "json", "-o", file_out], shell=True, check=True, capture_output=True, text=True, cwd=output_dir)
         return result.stdout
     except subprocess.CalledProcessError as e:
-        print("Error:", e.stderr)
+        print("CalledProcessError Error ==== :", e)
+
+def text_to_json_objects(text: str) -> List[dict]:
+    json_objects = []
+    if not text:
+        return json_objects
+    try:
+        json_objects = json.loads(text)
+        if isinstance(json_objects, dict):
+            json_objects = [json_objects]
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
+    return json_objects
+
+def any_spec_res(jsons: List[dict]) -> List[str]:
+    results = []
+    for item in jsons:
+        result = {}
+        if 'severity' in item:
+            result['severity'] = item['severity']
+        if 'code' in item and 'message' in item:
+            result['code'] = item['code']
+            result['message'] = item['message']
+        if result:
+            results.append(result)
+
+def anyYmlFile(out_dir: str) -> List[str]:
+    api_folder = pathlib.Path(out_dir)
+    results=[]
+    for api_file in api_folder.glob("*"):
+        if api_file.suffix == ".yml":
+            check_res=run_npm_script(api_file, api_folder)
+            # print(check_res)
+            k = text_to_json_objects(check_res)
+            print(k)
+            res = any_spec_res(k)
+            print(res)
+            results.append(res)
+    return results
 
 def generateSpec(srcDir: str, outDir: str) -> None:
     api_folder = pathlib.Path(srcDir)
@@ -84,15 +139,16 @@ def generateSpec(srcDir: str, outDir: str) -> None:
         filename = f"{api_file.stem}_response.yml"
         output_file = output_dir / filename
         with open(output_file, "w", encoding="utf-8") as out_file:
-            out_file.write(response["response"])
+            out_file.write(remove_code_block_format(response["response"]))
         check_res=run_npm_script(filename, outDir)
-        output = {
-            "response": response["response"],
-            "context": file_content,
-            "checkRes": check_res
-        }
-        with open(output_result, "a", encoding="utf-8") as jsonl_file:
-            jsonl_file.write(json.dumps(output) + "\n")
+        any_spec_res(text_to_json_objects(check_res))
+        # output = {
+        #     "response": response["response"],
+        #     "context": file_content,
+        #     "checkRes": check_res
+        # }
+        # with open(output_result, "a", encoding="utf-8") as jsonl_file:
+        #     jsonl_file.write(json.dumps(output) + "\n")
 
 def generateOutputData(srcDir: str, specDir: str, outDir: str) -> dict:
     api_folder = pathlib.Path(srcDir)
@@ -124,12 +180,9 @@ def generateOutputData(srcDir: str, specDir: str, outDir: str) -> dict:
 
 # Main function
 if __name__ == "__main__":
-    import os
-
-    user_home = os.getenv('AZURE_OPENAI_ENDPOINT')
-    print(user_home)
-    api_folder = pathlib.Path(__file__).parent.resolve() / "../apisrc"
+    # api_folder = pathlib.Path(__file__).parent.resolve() / "../apisrc"
     output_dir = pathlib.Path(__file__).parent.resolve() / "../output"
-    output_dir.mkdir(exist_ok=True)
-    apispec_dir = pathlib.Path(__file__).parent.resolve() / "../apispec"
-    generateSpec(api_folder, output_dir)
+    # output_dir.mkdir(exist_ok=True)
+    # apispec_dir = pathlib.Path(__file__).parent.resolve() / "../apispec"
+    # generateSpec(api_folder, output_dir)
+    print(anyYmlFile(output_dir))
