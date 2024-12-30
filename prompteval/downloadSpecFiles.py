d9bb470460b95cfc7169b97dd8ea6e25c2577447
wenyutang-ms <wenyutang@microsoft.com>
2024-12-30 15:51:57 +0800

    ci: update

--- prompteval/downloadSpecFiles.py
+++ prompteval/downloadSpecFiles.py
@@ -8,12 +8,10 @@ def downloadApiSpecFiles(folder_path: str):
     blob_service_client = BlobServiceClient(account_url=os.getenv('AZURE_STORAGE_ACCOUNT_URL'), credential=credential)
     # 下载文件
     container_name = os.getenv('AZURE_STORAGE_CONTAINER_NAME')
-    path = pathlib.Path(__file__).parent.resolve()
-    download_folder_path = path / ".." / folder_path
     container_client = blob_service_client.get_container_client(container_name)
     for blob in container_client.list_blobs():
         blob_client = container_client.get_blob_client(blob.name)
-        download_file_path = os.path.join(download_folder_path, blob.name)
+        download_file_path = os.path.join(folder_path, blob.name)
 
         # 创建下载文件的目录（如果不存在）
         os.makedirs(os.path.dirname(download_file_path), exist_ok=True)
@@ -25,5 +23,6 @@ def downloadApiSpecFiles(folder_path: str):
         print(f"Blob {blob.name} downloaded to {download_file_path}")
 
 if __name__ == "__main__":
-    api_func_folder = pathlib.Path('./apisrc')
-    downloadApiSpecFiles("apisrc")
+    current_path = pathlib.Path(__file__).parent.resolve()
+    api_func_folder = pathlib.Path(current_path, '../apisrc')
+    downloadApiSpecFiles(api_func_folder)
