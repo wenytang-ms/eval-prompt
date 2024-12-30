4a71ac71eb8e2c30fa20130d210ba5d4caa0c004
wenyutang-ms <wenyutang@microsoft.com>
2024-12-30 11:48:15 +0800

    perf: eval

--- prompteval/analyse.py
+++ prompteval/analyse.py
@@ -2,6 +2,8 @@ import subprocess
 import pathlib
 from typing import List, Tuple
 import json
+import os
+import pprint
 def spectral_analyse(filename: str, ruleset_url: str, output_dir: str, file_out: str) -> str:
     print("src file name: ", filename)
     print("out file name: ", file_out)
@@ -33,22 +35,47 @@ def text_to_json_objects(api_folder: str, file: str) -> List[dict]:
         print(f"Error decoding JSON: {e}")
     return json_objects
 
-def any_spec_res(jsons: List[dict]) -> Tuple[List[dict], dict]:
-    results = []
-    calc = {'warn': 0, 'error': 0, 'info': 0}
+class specRes:
+    def __init__(self, severity, code, message):
+        self.severity = severity
+        self.code = code
+        self.message = message
+
+    def __eq__(self, other):
+        if isinstance(other, specRes):
+            return self.severity == other.severity and self.code == other.code and self.message == other.message
+        return False
+
+    def __hash__(self):
+        return hash(self.severity) ^ hash(self.code) ^ hash(self.message)
+    
+    def __str__(self):
+        return f"Severity: {self.severity}, Code: {self.code}, Message: {self.message}"
+    
+    def __repr__(self):
+        return self.__str__()
+    
+    def to_json(self):
+        return {
+            "severity": self.severity,
+            "code": self.code,
+            "message": self.message
+        }
+
+def any_spec_res(jsons: List[dict]) -> Tuple[set[dict], dict]:
+    unique_results = set()
     for item in jsons:
-        result = {}
         if 'severity' in item and 'code' in item and 'message' in item:
-            result['severity'] = item['severity']
-            result['code'] = item['code']
-            result['message'] = item['message']
-        if result:
-            if result['severity'] == 0:
-                calc['error'] += 1
-            elif result['severity'] == 1:
-                calc['warn'] += 1
-            elif result['severity'] == 2:
-                calc['info'] += 1
-            if result not in results:
-                results.append(result)
-    return results, calc
+            item = specRes(item['severity'], item['code'], item['message'])
+            if item not in unique_results:
+                unique_results.add(item)
+    return unique_results
+
+if __name__ == "__main__":
+    out_file = "pet_response_output.json"
+    res_file = "pet_response.yml"
+    path = pathlib.Path(__file__).parent.resolve()
+    ruleset_url = "https://raw.githubusercontent.com/Azure/APICenter-Analyzer/preview/resources/rulesets/oas.yaml"
+    spectral_analyse(res_file, ruleset_url, pathlib.Path(path / "../results"), out_file)
+    res, calc = any_spec_res(text_to_json_objects(pathlib.Path(path / "../results"), out_file))
+    pprint.pprint(res)
